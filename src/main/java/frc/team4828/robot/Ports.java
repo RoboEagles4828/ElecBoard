3b563aecf505c2db97f09652e03b9e43f389a13e
Jackson McClintock <jacksondm33@gmail.com>
2018-01-20 17:52:50 -0500

    Code format changes
    Should not affect anything

--- src/main/java/frc/team4828/robot/Ports.java
+++ src/main/java/frc/team4828/robot/Ports.java
@@ -1,15 +1,16 @@
 package frc.team4828.robot;
 
 public class Ports {
-	public static final int FRONT_LEFT = 1;
-	public static final int FRONT_RIGHT = 2;
-	public static final int BACK_LEFT = 3;
-	public static final int BACK_RIGHT = 4;
 
-	public static final int JOYSTICK = 0;
+    public static final int FRONT_LEFT = 1;
+    public static final int FRONT_RIGHT = 2;
+    public static final int BACK_LEFT = 3;
+    public static final int BACK_RIGHT = 4;
 
-	// Pneumatics
-	public static final int COMPRESSOR = 7;
-	public static final int SOLENOID_LEFT = 0;
-	public static final int SOLENOID_RIGHT = 1;
+    public static final int JOYSTICK = 0;
+
+    // Pneumatics
+    public static final int COMPRESSOR = 7;
+    public static final int SOLENOID_LEFT = 0;
+    public static final int SOLENOID_RIGHT = 1;
 }
--- src/main/java/frc/team4828/robot/Tester.java
+++ src/main/java/frc/team4828/robot/Tester.java
@@ -6,28 +6,30 @@ import edu.wpi.first.wpilibj.Timer;
 import static com.ctre.phoenix.motorcontrol.ControlMode.*;
 
 public class Tester {
-	public void testMotors(TalonSRX m1, TalonSRX m2, TalonSRX m3, TalonSRX m4, double speed) {
-		m1.set(PercentOutput,speed);
-		m1.set(PercentOutput,speed);
-		Timer.delay(1);
-		m2.set(PercentOutput,speed);
-		Timer.delay(1);
-		m3.set(PercentOutput,speed);
-		Timer.delay(1);
-		m4.set(PercentOutput,speed);
-		Timer.delay(1);
-		m1.set(PercentOutput,-speed);
-		Timer.delay(1);
-		m2.set(PercentOutput,-speed);
-		Timer.delay(1);
-		m3.set(PercentOutput,-speed);
-		Timer.delay(1);
-		m4.set(PercentOutput,-speed);
-		Timer.delay(1);
-		m1.set(PercentOutput,0);
-		m2.set(PercentOutput,0);
-		m3.set(PercentOutput,0);
-		m4.set(PercentOutput,0);
-		System.out.println("Finish testing motors");
-	}
+
+    public void testMotors(TalonSRX m1, TalonSRX m2, TalonSRX m3, TalonSRX m4, double speed) {
+        m1.set(PercentOutput, speed);
+        m1.set(PercentOutput, speed);
+        Timer.delay(1);
+        m2.set(PercentOutput, speed);
+        Timer.delay(1);
+        m3.set(PercentOutput, speed);
+        Timer.delay(1);
+        m4.set(PercentOutput, speed);
+        Timer.delay(1);
+        m1.set(PercentOutput, -speed);
+        Timer.delay(1);
+        m2.set(PercentOutput, -speed);
+        Timer.delay(1);
+        m3.set(PercentOutput, -speed);
+        Timer.delay(1);
+        m4.set(PercentOutput, -speed);
+        Timer.delay(1);
+        m1.set(PercentOutput, 0);
+        m2.set(PercentOutput, 0);
+        m3.set(PercentOutput, 0);
+        m4.set(PercentOutput, 0);
+        System.out.println("Finish testing motors");
+    }
+
 }
